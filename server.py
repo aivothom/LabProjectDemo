264a190706882d82f68abb91c67d579185a5f0fe
thomasvo <thomasvo@buffalo.edu>
2019-03-05 18:12:13 -0500

    New Repo w/ flask

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
