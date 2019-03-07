ceeeede07b95933cfc704b28aeadbb04d4c21496
thomasvo <thomasvo@buffalo.edu>
2019-03-07 17:46:07 -0500

    Merge branch 'master' of /Users/thomasvo/IdeaProjects/ServerLabProject with conflicts.

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-import sqlite3
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
+#routes 'index.html' to / on webpage
 def index():
     return render_template('index.html')
 
@@ -15,9 +16,28 @@ def game():
 
 
 @app.route('/login')
+#routes 'login.html' to /login on webpage
 def login():
     return render_template('login.html')
 
 
+#creating a json file for all of the usernames, score, and location
+def toJson():
+    USERNAME = "aivothom"
+    SCORE = 86
+    x = 0
+    y = 0
+    data = {
+        'name': USERNAME,
+        'score': SCORE,
+        'location': {'x': x, 'y': y}}
+    return json.dumps(data)
+
+
+#taking the created json file, reading it and returning the data
+def fromJson():
+    return 0.0
+
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", "8080")
+    app.run(host='0.0.0.0')
